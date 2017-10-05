485addd4e0fb7ac18eacc7ebb5d5d6d6c596e97a
andreblgithub <andrewbl@icloud.com>
2017-10-05 16:36:21 -0400

    title fixed

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Filesizer' });
 });
 
 router.post('/', upload.single('fileupload'), function(req, res, next) {
