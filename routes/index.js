de236a013fe60c94c44f2e26feed6ba8dceffcc7
andreblgithub <andrewbl@icloud.com>
2017-10-05 16:33:39 -0400

    app ready

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ storage: multer.memoryStorage() });
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', upload.single('fileupload'), function(req, res, next) {
+  var fileSize = req.file.size;
+  var fileSizeFormatted = { "FileSizeBytes":fileSize };
+  console.log(fileSizeFormatted);
+  res.json(fileSizeFormatted);
+})
+
+function getFileSize(file) {
+  var stats = fs.statSync(file);
+  return stats.size;
+}
 
 module.exports = router;
